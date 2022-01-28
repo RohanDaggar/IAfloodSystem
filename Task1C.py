f392df584d3202660417c6679405594e1cd8d84c
DrDagz <dagzthegreat@gmail.com>
2022-01-28 21:03:24 +0000

    completed task 1C

--- Task1C.py
+++ Task1C.py
@@ -1,1 +1,9 @@
-print("Rohans file")
+from floodsystem.geo import stations_within_radius
+from floodsystem.stationdata import build_station_list
+    
+centre = (52.2053, 0.1218)
+stations = build_station_list()
+r = 10
+less_than_10 = stations_within_radius(stations, centre, r)
+
+print(less_than_10)
