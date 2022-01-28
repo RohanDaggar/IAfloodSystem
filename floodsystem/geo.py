c1bba0f4268f3ea5b58956770db7de578fd6ade1
DrDagz <dagzthegreat@gmail.com>
2022-01-28 21:00:26 +0000

    added task 1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,8 +7,23 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+from math import sqrt
 
-print("random")
 
-def Func():
-    pass
+def stations_within_radius(stations, centre, r):
+    """Prints an alphabetic list of the stations within a radius 'r' around a centre 'centre'
+
+    Args:
+        stations (list): list of the stations using the station class
+        centre (tuple): centre co-ordinate descibed in a tuple (x,y)
+        r (integer): radius r given in km
+    """
+    
+    less_than_10 = []
+    for station in stations:
+        distance = haversine(station.coord, centre)
+        if distance <= r:
+               less_than_10.append(station.name)
+    return less_than_10
+    
