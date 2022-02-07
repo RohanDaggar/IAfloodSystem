450cfd9e76d0c344f1ed3a58c3619fdcbd71fcad
jjt53 <jjt53@cam.ac.uk>
2022-02-07 15:01:24 +0000

    try

--- Task1B.py
+++ Task1B.py
@@ -5,7 +5,8 @@ def run():
     stations = build_station_list()
     a = stations_by_distance(stations,(52.2053,0.1218))
     print(a[:10])
-    print(a[-10:])
+    b=a[::-1]
+    print(b[:10])
 
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
