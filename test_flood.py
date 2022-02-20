130b57cbd6be42886fce0641f64565015cfe2a43
RohanDaggar <dagzthegreat@gmail.com>
2022-02-20 18:35:18 +0000

    final tests done

--- test_flood.py
+++ test_flood.py
@@ -1,24 +1,36 @@
 from floodsystem.station import MonitoringStation
+from floodsystem.flood import stations_level_over_threshold
 
 def test_stations_level_over_threshold():
-    s_id = "test-s-id"
-    m_id = "test-m-id"
-    label = "some station"
-    coord = (-2.0, 4.0)
-    trange = (-2.3, 2.384)
-    river = "River X"
-    town = "My Town"
-    latest_level = 6
-    s1 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
-    s1.latest_level = latest_level
+    s_id1 = "test-s-id"
+    m_id1 = "test-m-id"
+    label1 = "some station"
+    coord1 = (-2.0, 4.0)
+    trange1 = (-2.3, 2.384)
+    river1 = "River X"
+    town1 = "My Town"
+    latest_level1 = 6
+    s1 = MonitoringStation(s_id1, m_id1, label1, coord1, trange1, river1, town1)
+    s1.latest_level = latest_level1
     
-    s_id = "test-s-id"
-    m_id = "test-m-id"
-    label = "some station"
-    coord = (-2.0, 4.0)
-    trange = (-2.3, 2.384)
-    river = "River X"
-    town = "My Town"
-    latest_level = 0
-    s2 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+    s_id2 = "test-s-id"
+    m_id2 = "test-m-id"
+    label2 = "some station"
+    coord2 = (-2.0, 4.0)
+    trange2 = (-2.3, 2.384)
+    river2 = "River X"
+    town2 = "My Town"
+    latest_level2 = 0
+    s2 = MonitoringStation(s_id2, m_id2, label2, coord2, trange2, river2, town2)
+    s2.latest_level = latest_level2
     
+    stations = [s1, s2]
+    assert len(stations_level_over_threshold(stations, 100)) == 0
+    assert len(stations_level_over_threshold(stations, -100)) == 2
+    assert len(stations_level_over_threshold(stations, 0.8)) == 1
+    test = stations_level_over_threshold(stations, 0)
+    for item in test:
+        assert type(item) is tuple
+        assert len(item) == 2
+        assert type(item[0]) is MonitoringStation
+        assert type(item[1]) is float
