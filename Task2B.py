59647d2ec44f31309357d49a8728a6119e568257
RohanDaggar <dagzthegreat@gmail.com>
2022-02-20 18:35:27 +0000

    finished the task

--- Task2B.py
+++ Task2B.py
@@ -1,10 +1,13 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
+
 def run():
     stations = build_station_list()
+    update_water_levels(stations)
     
-    for station, relative_water_level in stations_level_over_threshold(stations, 0.8):
-        print(station)
+    for item in stations_level_over_threshold(stations, 0.8):
+        station, level = item
+        print(station.name, level)
 
 if __name__ == "__main__":
     print("*** Task 2B: CUED Part IA Flood Warning System ***")
