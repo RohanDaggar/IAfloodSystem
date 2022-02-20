88964eb8d2046968ee29e20c47c82faad2e1d048
RohanDaggar <dagzthegreat@gmail.com>
2022-02-20 19:27:09 +0000

    completed 2C

--- Task2C.py
+++ Task2C.py
@@ -1,5 +1,12 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+
 def run():
-    pass
+    stations = build_station_list()
+    update_water_levels(stations)
+    
+    for station in stations_highest_rel_level(stations, 10):
+        print(f'{station.name} {station.relative_water_level()}')
 
 if __name__ == "__main__":
     print("*** Task 2C: CUED Part IA Flood Warning System ***")
