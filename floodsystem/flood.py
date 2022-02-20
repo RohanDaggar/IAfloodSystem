6b3d2fcd7bc64f42988989e7088c30ed327f57bf
RohanDaggar <dagzthegreat@gmail.com>
2022-02-20 18:35:42 +0000

    finished the function

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,15 +1,17 @@
-from station import check_stations_input
-from stationdata import update_water_levels
+from floodsystem.station import check_stations_input
+from floodsystem.utils import sorted_by_key
 
 def stations_level_over_threshold(stations, tol):
+    #assumes that station objects have updates water levels already
     check_stations_input(stations)
     assert type(tol) is int or type(tol) is float
     
-    update_water_levels(stations)
-    
     rlist = [] #(station, relative water level) for stations above the tolerance
     for station in stations:
-        if station.relative_water_level() > tol:
-            rlist.append(station, station.relative_water_level())
-        
-    return rlist
+        try:
+            if station.relative_water_level() > tol:
+                rlist.append((station, station.relative_water_level()))
+        except TypeError:
+            pass
+    
+    return sorted_by_key(rlist, 1, reverse=True)
