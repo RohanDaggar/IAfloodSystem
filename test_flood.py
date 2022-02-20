b971dea1f38c0ac4d513148d94e7107ec615f0bf
RohanDaggar <dagzthegreat@gmail.com>
2022-02-20 19:26:59 +0000

    added a test for 2C

--- test_flood.py
+++ test_flood.py
@@ -1,5 +1,8 @@
 from floodsystem.station import MonitoringStation
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
+stations = build_station_list()
+update_water_levels(stations)
 
 def test_stations_level_over_threshold():
     s_id1 = "test-s-id"
@@ -34,3 +37,9 @@ def test_stations_level_over_threshold():
         assert len(item) == 2
         assert type(item[0]) is MonitoringStation
         assert type(item[1]) is float
+        
+def test_stations_highest_rel_level():
+    station_list = stations_highest_rel_level(stations, 10)
+    assert len(station_list) == 10
+    for station in station_list:
+        assert type(station) is MonitoringStation
