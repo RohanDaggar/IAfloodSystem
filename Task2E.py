11d24165855c57b6e970e7e9c34bd302579e8b64
jjt53 <jjt53@cam.ac.uk>
2022-02-26 19:29:14 +0000

    :))

--- Task2E.py
+++ Task2E.py
@@ -7,8 +7,10 @@ import numpy as np
 from datetime import timedelta  
 
 
-def run(station):
-    
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+    station = stations_highest_rel_level(stations, 5)
 
     #a = []
     #station_name = []
@@ -47,14 +49,10 @@ def run(station):
     #print(d.flatten()[:,1])
     #plot_water_levels(station, dates, levels)
     for s in station:
-        dates, levels = fetch_measure_levels(station.measure_id,
+        dates, levels = fetch_measure_levels(station[s].measure_id,
                                      dt=datetime.timedelta(days=10))
         plot_water_levels(s,dates,levels)
        
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
-    
-stations = build_station_list()
-update_water_levels(stations)
-station = stations_highest_rel_level(stations, 5)
-run(station)    
+    run()    
