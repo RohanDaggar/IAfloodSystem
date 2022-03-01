a41cfc7b31a4c6e51acbe83c0f31d2c944a26c1f
jjt53 <jjt53@cam.ac.uk>
2022-03-01 11:14:31 +0000

    o

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -4,6 +4,8 @@ from floodsystem.stationdata import build_station_list
 def plot_water_levels(station, dates, levels):
     
     plt.plot(dates, levels)
+    plt.axhline(y=0, label="Typical Low")
+    plt.axhline(y=1, label="Typical High")
     
     plt.title('stations.name')
     plt.xlabel('Date')
